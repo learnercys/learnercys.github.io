d89082d1095c74de3d1bede28b65abb6ccaa8ff8
Carlos Hernandez <learnercys@gmail.com>
2017-12-08 00:53:48 -0600

    [develop] Open external links with `new tab` or `new window` (a.k.a. target=`blank`)

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,10 +7,10 @@ class Contact extends Component {
             <div className={styles.container}>
                 <h3>Contact</h3>
                 <ul>
-                    <li><a href="mailto:learnercys@gmail.com">learnercys@gmail.com</a></li>
-                    <li><a href="https://github.com/learnercys">github</a></li>
-                    <li><a href="https://twitter.com/learnercys">twitter</a></li>
-                    <li><a href="https://www.linkedin.com/in/carlos-hernandez-1571b187/">linkedin</a></li>
+                    <li><a target="_blank" href="mailto:learnercys@gmail.com">learnercys@gmail.com</a></li>
+                    <li><a target="_blank" href="https://github.com/learnercys">github</a></li>
+                    <li><a target="_blank" href="https://twitter.com/learnercys">twitter</a></li>
+                    <li><a target="_blank" href="https://www.linkedin.com/in/carlos-hernandez-1571b187/">linkedin</a></li>
                 </ul>
             </div>
         );
