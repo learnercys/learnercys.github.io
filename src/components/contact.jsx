31a9cdadb5518f21bddfbddc656364ff95fb34c6
Carlos Hernandez <chernandez@cognits.co>
2020-04-22 13:18:48 -0600

    Delete github

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -15,7 +15,6 @@ class Contact extends Component {
                 <h3>Contact</h3>
                 <ul>
                     <li><i className="fa fa-envelope-o"></i> <a target="_blank" href="mailto:learnercys@gmail.com">learnercys@gmail.com</a></li>
-                    <li><i className="fa fa-github"></i> <a target="_blank" href="https://github.com/learnercys">github</a></li>
                     <li><i className="fa fa-linkedin-square"></i> <a target="_blank" href="https://www.linkedin.com/in/carlos-hernandez-1571b187/">linkedin</a></li>
                 </ul>
             </div>
