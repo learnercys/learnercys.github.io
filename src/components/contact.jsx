d707598d0034aa8482859e38ffbd7291eb1d6b34
Carlos Hernandez <xyc.hernandez@gmail.com>
2020-05-11 09:56:07 -0600

    Fix email

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -14,7 +14,7 @@ class Contact extends Component {
                 </ul>
                 <h3>Contact</h3>
                 <ul>
-                    <li><i className="fa fa-envelope-o"></i> <a target="_blank" href="mailto:learnercys@gmail.com">learnercys@gmail.com</a></li>
+                    <li><i className="fa fa-envelope-o"></i> <a target="_blank" href="mailto:xyc.hernandez@gmail.com">xyc.hernandez@gmail.com</a></li>
                     <li><i className="fa fa-linkedin-square"></i> <a target="_blank" href="https://www.linkedin.com/in/carlos-hernandez-1571b187/">linkedin</a></li>
                 </ul>
             </div>
