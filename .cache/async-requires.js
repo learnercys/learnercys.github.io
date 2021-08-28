ecdb733684162f4edcf67f510b17a5e131ab5f8c
Carlos Hernandez <xyc.hernandez@gmail.com>
2021-08-28 17:45:50 -0600

    Release version

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,25 +2,16 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/dev-404-page.js"),
-  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/learnercys/Documents/gh/learnercys.github.io/src/pages/404.js"),
-  "component---src-pages-from-the-beginning-js": require("gatsby-module-loader?name=component---src-pages-from-the-beginning-js!/Users/learnercys/Documents/gh/learnercys.github.io/src/pages/from-the-beginning.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/learnercys/Documents/gh/learnercys.github.io/src/pages/index.js"),
-  "component---src-pages-resume-js": require("gatsby-module-loader?name=component---src-pages-resume-js!/Users/learnercys/Documents/gh/learnercys.github.io/src/pages/resume.js"),
-  "component---src-pages-test-a-node-rest-api-js": require("gatsby-module-loader?name=component---src-pages-test-a-node-rest-api-js!/Users/learnercys/Documents/gh/learnercys.github.io/src/pages/test-a-node-rest-api.js")
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/cxy/Documents/gh/learnercys.github.io/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/cxy/Documents/gh/learnercys.github.io/src/pages/404.js")
 }
 
 exports.json = {
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/dev-404-page.json"),
-  "404.json": require("gatsby-module-loader?name=path---404!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/404.json"),
-  "from-the-beginning.json": require("gatsby-module-loader?name=path---from-the-beginning!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/from-the-beginning.json"),
-  "index.json": require("gatsby-module-loader?name=path---index!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/index.json"),
-  "resume.json": require("gatsby-module-loader?name=path---resume!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/resume.json"),
-  "test-a-node-rest-api.json": require("gatsby-module-loader?name=path---test-a-node-rest-api!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/test-a-node-rest-api.json"),
-  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/404-html.json")
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cxy/Documents/gh/learnercys.github.io/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/cxy/Documents/gh/learnercys.github.io/.cache/json/dev-404-page.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/cxy/Documents/gh/learnercys.github.io/.cache/json/404-html.json")
 }
 
 exports.layouts = {
-  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/learnercys/Documents/gh/learnercys.github.io/.cache/layouts/index.js")
+  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/cxy/Documents/gh/learnercys.github.io/.cache/layouts/index.js")
 }
