0651baa35ebbd3c39c6534c31ac744e7d072d292
Carlos Hernandez <xyc.hernandez@gmail.com>
2021-08-28 18:03:05 -0600

    Release version

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/learnercys/Documents/gh/learnercys.github.io/src/layouts/index.js"
-  import data from "/Users/learnercys/Documents/gh/learnercys.github.io/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
