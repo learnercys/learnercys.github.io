2d9150f3207c40dada0fd1605e3b3983a7fd992e
Carlos Hernandez <xyc.hernandez@gmail.com>
2022-08-28 16:01:15 -0600

    Release Version

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,1 +1,6 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+
 import './src/styles/global.scss'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
